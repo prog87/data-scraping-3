3f29ab70be6d1ea90966a3f68a9132eaa21d61fb
Rizky Rahman <rizkpython87@gmail.com>
2023-05-24 09:20:53 +0800

    bad request html 404

--- run.py
+++ run.py
@@ -1,14 +1,37 @@
 import requests
 from bs4 import BeautifulSoup
-
+# jangan di ketik from flask import flask
 from flask import *
 
 app = Flask(__name__)
 @app.route('/')
 def home():
-    return 'Test'
+    # return 'Test'
+    return render_template('index.html')
+# masuk ke terminal dan jalankan python run.py
+# untuk return 'Test' ketika sudah masuk local host maka akan muncul Test
+# kemudian dirubah menjadi return render_template('index.html')
+# Buat direktori template
+# setelah itu buat file baru index.html
 
+# kita gunakan bootsrap4
+# masuk ke navbar brand/logo
+# copy dan masuk ke index.html
 
+# Buat route dan fungsi
+@app.route('/news-popular')
+def news_popular():
+    html_doc = requests.get('https://www.detik.com/search/searchall?', params={
+        'query': 'detik terpopuler',
+        'siteid': '2'
+    })
+    soup = BeautifulSoup(html_doc.text, 'html.parser')
+    news_populer = soup.find(attrs={'class': 'list media_rows list-berita'})
+
+    titles = news_populer.find_all(attrs={'class': 'title'})
+    images = news_populer.find_all(attrs={'class': 'ratiobox box_thumb'})
+
+    return render_template('index.html', images=images)
 
 
 
